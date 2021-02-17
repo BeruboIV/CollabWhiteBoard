24a70ba3139cadeee1912d975a99aee3fc2cea06
BeruboIV <hardikraj13@gmail.com>
2021-02-18 00:07:59 +0530

    Added cursor follow erase

--- whiteboard.js
+++ whiteboard.js
@@ -3,14 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
   var context = canvas.getContext("2d");
   const option = document.querySelectorAll(".btn");
   var canvasWrapper = document.getElementById("canvas-wrapper").getBoundingClientRect();
+  const cursor = document.querySelector(".cursor");
+  var current_mode = "pencil";
 
   canvas.width = canvasWrapper.width;
   canvas.height = canvasWrapper.height;
   /* This causes the canvas to refresh but it will work for now.*/
-  window.onresize = function(){
-    canvas.width = canvasWrapper.width;
-    canvas.height = canvasWrapper.height;
-  }
+  // window.onresize = function () {
+  //   canvas.width = canvasWrapper.width;
+  //   canvas.height = canvasWrapper.height;
+  // };
   var dragging = false;
   var radius = 5;
 
@@ -23,13 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
     context.stroke();
     context.beginPath();
     //   context.arc(e.clientX, e.clientY, 5, 0, 2 * Math.PI); // Was giving a lot of issues
-    context.arc(
-      X,
-      Y,
-      radius,
-      0,
-      2 * Math.PI
-    );
+    context.arc(X, Y, radius, 0, 2 * Math.PI);
     context.fill();
     context.beginPath();
     context.moveTo(X, Y);
@@ -55,28 +51,30 @@ document.addEventListener("DOMContentLoaded", function () {
       if (option[i].classList.contains("eraser")) {
         // The existing content is kept where it doesn't overlap the new shape.
         context.globalCompositeOperation = "destination-out";
-        document.body.style.cursor = "auto"
+        document.body.style.cursor = "auto";
+        current_mode = "erase";
+      }
+      else{
+        current_mode = "pencil";
       }
       if (option[i].classList.contains("pencil")) {
-        // This is the default setting and draws new shapes on top of the existing canvas content.
-        context.globalCompositeOperation = "source-over";
       }
-      if (option[i].classList.contains("red")) {
+      else if (option[i].classList.contains("red")) {
         context.fillStyle = "red";
         context.strokeStyle = "red";
         context.globalCompositeOperation = "source-over";
       }
-      if (option[i].classList.contains("blue")) {
+      else if (option[i].classList.contains("blue")) {
         context.fillStyle = "blue";
         context.strokeStyle = "blue";
         context.globalCompositeOperation = "source-over";
       }
-      if (option[i].classList.contains("green")) {
+      else if (option[i].classList.contains("green")) {
         context.fillStyle = "green";
         context.strokeStyle = "green";
         context.globalCompositeOperation = "source-over";
       }
-      if (option[i].classList.contains("black")) {
+      else if (option[i].classList.contains("black")) {
         context.fillStyle = "black";
         context.strokeStyle = "black";
         context.globalCompositeOperation = "source-over";
@@ -84,4 +82,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  document.addEventListener("mousemove", e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - scrollY) + "px; left: " + (e.pageX) + "px;");
+    if(current_mode == "erase")
+      cursor.style.backgroundColor = "black";
+    else
+      cursor.style.opacity = 0;
+  })
+
 });
